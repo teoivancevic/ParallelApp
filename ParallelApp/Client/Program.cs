6b79ad6a8e11d73e4841b643f1dddf01be29d6a6
dsabolic <domagojsabolic2003@gmail.com>
2022-04-25 23:01:02 +0200

    Dodavanje podrške za AAIEdu identitet SSO

--- ParallelApp/Client/Program.cs
+++ ParallelApp/Client/Program.cs
@@ -18,8 +18,15 @@ builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(build
 
 builder.Services.AddOidcAuthentication(options =>
 {
-    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    builder.Configuration.Bind("AAIEdu", options.ProviderOptions);
     options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.DefaultScopes.Add("openid");
+    options.ProviderOptions.DefaultScopes.Add("profile");
+    options.ProviderOptions.DefaultScopes.Add("hrEduPersonAffiliation");
+    options.ProviderOptions.DefaultScopes.Add("hrEduPersonGroupMember");
+    options.ProviderOptions.DefaultScopes.Add("hrEduPersonPersistentID");
+    options.ProviderOptions.DefaultScopes.Add("o");
+    options.ProviderOptions.DefaultScopes.Add("sn");
 });
 
 
