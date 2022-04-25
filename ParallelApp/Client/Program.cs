f519bff17cd967e77fe3d185a9b19f746d2d1a0c
dsabolic <domagojsabolic2003@gmail.com>
2022-04-26 01:17:08 +0200

    Revert "Dodavanje podrške za AAIEdu identitet SSO"
    This reverts commit 6b79ad6a8e11d73e4841b643f1dddf01be29d6a6.

--- ParallelApp/Client/Program.cs
+++ ParallelApp/Client/Program.cs
@@ -18,15 +18,8 @@ builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(build
 
 builder.Services.AddOidcAuthentication(options =>
 {
-    builder.Configuration.Bind("AAIEdu", options.ProviderOptions);
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
     options.ProviderOptions.ResponseType = "code";
-    options.ProviderOptions.DefaultScopes.Add("openid");
-    options.ProviderOptions.DefaultScopes.Add("profile");
-    options.ProviderOptions.DefaultScopes.Add("hrEduPersonAffiliation");
-    options.ProviderOptions.DefaultScopes.Add("hrEduPersonGroupMember");
-    options.ProviderOptions.DefaultScopes.Add("hrEduPersonPersistentID");
-    options.ProviderOptions.DefaultScopes.Add("o");
-    options.ProviderOptions.DefaultScopes.Add("sn");
 });
 
 
